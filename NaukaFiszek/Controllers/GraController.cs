9086083c71ad8f1c0af602c28af61b0d944d1142
Jarosław Gosławski <jarek93pl@hotmail.com>
2019-10-10 17:27:10 +0200

    dodanie widoku prompt

---
+++ NaukaFiszek/Controllers/GraController.cs
@@ -0,0 +1,33 @@
+﻿using NaukaFiszek.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NaukaFiszek.Controllers
+{
+    public class GraController : Controller
+    {
+        [HttpGet]
+        public ActionResult Prompt(DTO.Fiche fiche)
+        {
+            fiche = new DTO.Fiche();
+            fiche.IdPromptFile = 1020;
+            fiche.TypePrompt = DTO.Enums.ContentType.Sound;
+            return View(fiche);
+        }
+        [HttpGet]
+        public ActionResult WriteText(int idFiche, bool isMultiPlayer = false)
+        {
+            using (Conector.Fiche fiche = new Conector.Fiche())
+            {
+                var currentFiche = fiche.LoadFiche(idFiche);
+                GameState game = new GameState();
+                game.IsMultiPlayer = isMultiPlayer;
+                return View();
+            }
+        }
+
+    }
+}
---
+++ NaukaFiszek/Models/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NaukaFiszek.Models
+{
+    public class GameState
+    {
+        public bool IsMultiPlayer { get; set; }
+        public DTO.Fiche Fiche { get; set; }
+    }
+}
